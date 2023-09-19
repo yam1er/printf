581bbac71bb08cd4643a91f0b79da543ca1088dc
AlphonseMehounme <alphonsemehounme9@gmail.com>
2023-09-18 18:08:29 -0700

    Correct betty error

--- _printf.c
+++ _printf.c
@@ -10,47 +10,47 @@
 
 int _printf(const char *format, ...)
 {
-    va_list args;
-    int count = 0;
-    char c, percent = '%';
-    char *str;
+	va_list args;
+	int count = 0;
+	char c, percent = '%';
+	char *str;
 
-    va_start(args, format);
-    while(*format != '\0')
-    {
-        if(*format == '%')
-        {
-            format++;
-            if (*format == 'c')
-            {
-                c = va_arg(args, int);
-                write(1, &c, 1);
-                count++;
-            } else if (*format == 's')
-            {
-                str = va_arg(args, char *);
-                while(*str != '\0')
-                {
-                    write(1, str, 1);
-                    str++;
-                    count++;
-                }
-            } else if (*format == '%')
-            {
-                write(1, &percent, 1);
-                count++;
-            } else
-            {
-                write(1, format, 1);
-                count++;
-            }
-        } else
-        {
-            write(1, format, 1);
-            count++;
-        }
-        format++;
-    }
-    va_end(args);
-    return count;
+	va_start(args, format);
+	while (*format != '\0')
+	{
+		if (*format == '%')
+		{
+			format++;
+			if (*format == 'c')
+			{
+				c = va_arg(args, int);
+				write(1, &c, 1);
+				count++;
+			} else if (*format == 's')
+			{
+				str = va_arg(args, char *);
+				while (*str != '\0')
+				{
+					write(1, str, 1);
+					str++;
+					count++;
+				}
+			} else if (*format == '%')
+			{
+				write(1, &percent, 1);
+				count++;
+			} else
+			{
+				write(1, format, 1);
+				count++;
+			}
+		} else
+		{
+			write(1, format, 1);
+			count++;
+		}
+		format++;
+	}
+	va_end(args);
+	return (count);
 }
